6385dc75b82499077f1fa62834a0fae15be89d82
Nikolai Gordienko <nicolas.gordienko@gmail.com>
2022-01-14 18:51:58 +0100

    fixed the game prime

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -10,10 +10,14 @@ public class Prime {
     public static String[][] getQuestionsAndAnswers() {
         String[][] questionsAndAnswers = new String[2][Engine.ITERATIONS_NUMBER];
         for (int i = 0; i < Engine.ITERATIONS_NUMBER; i++) {
-            int random = (int) (Math.random() * Engine.MAX_VALUE);
+            questionsAndAnswers[1][i] = "yes";
+            int random = (int) (1 +Math.random() * Engine.MAX_VALUE);
             questionsAndAnswers[0][i] = String.valueOf(random);
             for (int j = random - 1; j > 1; j--) {
-                questionsAndAnswers[1][i] = (random % j == 0) ? "no" : "yes";
+                if (random % j == 0) {
+                    questionsAndAnswers[1][i] = "no";
+                    break;
+                }
             }
         }
         return questionsAndAnswers;
